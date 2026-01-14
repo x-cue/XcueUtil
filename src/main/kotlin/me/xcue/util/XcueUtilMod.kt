6fd88bf01bf316ad5f499c5970b4d0dc611e9c39
x-cue <174210264+x-cue@users.noreply.github.com.>
2026-01-14 09:40:29 -0600

    Initial Commit

--- src/main/kotlin/me/xcue/util/XcueUtilMod.kt
+++ src/main/kotlin/me/xcue/util/XcueUtilMod.kt
@@ -1,24 +1,25 @@
-package com.example.mod
+package me.xcue.util
 
 import com.hypixel.hytale.logger.HytaleLogger
 import com.hypixel.hytale.server.core.plugin.JavaPlugin
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit
+import me.xcue.util.commands.HelloCommand
 
 /* This is the main class: the entry point for your plugin.
  * Use the setup function to register commands or event listeners.
  */
 
-class TestMod(init: JavaPluginInit) : JavaPlugin(init) {
+class XcueUtilMod(init: JavaPluginInit) : JavaPlugin(init) {
     companion object {
         private val LOGGER: HytaleLogger = HytaleLogger.forEnclosingClass()
     }
 
     init {
-        LOGGER.atInfo().log("Hello from ${this.name} version ${this.manifest.version}")
+
     }
 
     override fun setup() {
-        LOGGER.atInfo().log("Setting up plugin ${this.name}")
+        commandRegistry.registerCommand(HelloCommand())
     }
 }
 
---
+++ src/main/kotlin/me/xcue/util/commands/HelloCommand.kt
@@ -0,0 +1,16 @@
+package me.xcue.util.commands
+
+import com.hypixel.hytale.server.core.Message
+import com.hypixel.hytale.server.core.command.system.CommandContext
+import com.hypixel.hytale.server.core.command.system.basecommands.CommandBase
+import java.util.Random
+
+class HelloCommand : CommandBase("hello", "Receive a simple greeting!") {
+    val greetings = listOf("Hello.", "HI", "Welcome!")
+
+    override fun executeSync(ctx: CommandContext) {
+        val msg = Message.raw(greetings[Random().nextInt(0, greetings.size)])
+
+        ctx.sendMessage(msg)
+    }
+}
